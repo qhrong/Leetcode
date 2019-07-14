2c0e01a4ea67fba029b7bb731974dfbaf325791b
qhrong <qianhuirong320@gmail.com>
2019-07-13 20:29:11 -0400

    init commit

---
+++ Univalued Binary Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#      def __init__(self, x):
+#          self.val = x
+#          self.left = None
+#          self.right = None
+
+class Solution:
+    def isUnivalTree(self, root: TreeNode) -> bool:
+
+        if not root:
+            return True
+
+        if root:
+            if root.left:
+                if root.val != root.left.val:
+                    return False
+            if root.right:
+                if root.val != root.right.val:
+                    return False
+            return self.isUnivalTree(root.right) and self.isUnivalTree(root.left)
